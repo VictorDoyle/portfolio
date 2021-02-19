c21c3bb0fbe58b2fe1dffbe848ff17e4b58c5bac
victordoyle <victor-doyle@outlook.com>
2021-02-18 23:35:02 -0500

    fixed styling for Resume section

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Menu, Header, Image, Segment, Container } from 'semantic-ui-react'
+import { Menu, Header, Image, Tab, Container } from 'semantic-ui-react'
 import './Resume.css'
 
 
@@ -8,30 +8,43 @@ function Resume () {
     const [activeItem, setActiveItem] = useState("home")
     const handleItemClick = (e, { name }) => setActiveItem( name )
 
+    /* panel content */
+
+    const panes = [
+      { menuItem : 'General Assembly', render: () => <Tab.Pane className="tabContent">
+        
+        Tab 1 Content
+        
+        </Tab.Pane> },
+
+
+      { menuItem: 'Array Property Group', render: () => <Tab.Pane className="tabContent">
+        
+        Tab 2 Content
+        
+        </Tab.Pane> },
+
+
+      { menuItem: 'E-Commerce', render: () => <Tab.Pane className="tabContent">
+        
+        Tab 3 Content
+        
+        </Tab.Pane> },
+
+      { menuItem: 'Innovative Designs', render: () => <Tab.Pane className="tabContent">
+              
+      Tab 3 Content
+
+      </Tab.Pane> },
+
+
+    ]
+
     return(
         <>
   <Container text className="aboutSection" id="about"> 
-    <Header as='h2'>Where I've Worked <hr/></Header>
-  
-    <Menu pointing secondary vertical>
-        <Menu.Item
-          name='Array Property Group'
-          active={activeItem === 'Array Property Group'}
-          onClick={handleItemClick}
-        />
-
-
-        <Menu.Item
-          name='Freelance'
-          active={activeItem === 'Freelance'}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name='aqua fashion store'
-          active={activeItem === 'aqua fashion store'}
-          onClick={handleItemClick}
-        />
-      </Menu>
+    <Header> <h2 className="aboutHeader"> Technological Experience </h2> <hr/></Header>
+      <Tab className="menuItemPanel" menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
 
  
     </Container>
@@ -40,5 +53,6 @@ function Resume () {
 }
 
 export default Resume
+
 
 
