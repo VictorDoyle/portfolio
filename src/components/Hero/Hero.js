1a0ea553900f8ccb7e9d90720f4314a4e5c6fa9a
victordoyle <victor-doyle@outlook.com>
2021-06-18 18:12:12 -0400

    added profile image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,16 @@
 /* eslint-disable max-len */
 import React from 'react'
-import { Container, Header, Button, Icon, Label } from 'semantic-ui-react'
+import { Container, Header, Button, Image} from 'semantic-ui-react'
 import './Hero.css'
-
+import mainMeImage from '../../images/heroImage.JPEG'
 
 
 const Hero = () => (
   <Container text className="heroSection">
     <Header className="heroText1"> Hey, my name is </Header>
-    <Header className="heroText2"> Victor Doyle. </Header>
+    <Header className="heroText2"> Victor Doyle.  <Image src={mainMeImage} circular  centered/> </Header>
     <Header className="heroText3"> I Build Full Stack Applications. </Header>
+  {/*   <Image src={mainMeImage} circular size="small" centered/> */}
     <p className="heroText1">
     I'm a software engineer based in New York City. I specialize in Front End Development, building and designing unique websites and applications. I am also fluent in Back End Development and am in the midst of learning Java and C++.
     </p>
